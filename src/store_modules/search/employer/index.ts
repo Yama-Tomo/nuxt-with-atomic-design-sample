6aa3dac8f5fc969cdb9b31b983680a80e5140442
Yama-Tomo <mazda13bturbo@gmail.com>
2019-06-13 16:34:36 +0900

    refactor: default export以外のexportはアスタリスクで動的にexportできるように改善

--- src/store_modules/search/employer/index.ts
+++ src/store_modules/search/employer/index.ts
@@ -1,13 +1,8 @@
-import state, { IState, ChooseDataElement } from './state';
-import actions, { IActions } from './actions';
-import mutations, { IMutations } from './mutations';
+import state from './state';
+import actions from './actions';
+import mutations from './mutations';
+export * from './state';
+export * from './actions';
+export * from './mutations';
 
-export {
-  state,
-  actions,
-  mutations,
-  IState,
-  IActions,
-  IMutations,
-  ChooseDataElement,
-};
+export { state, actions, mutations };
