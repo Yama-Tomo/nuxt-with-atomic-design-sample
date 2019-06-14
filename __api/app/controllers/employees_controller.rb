f2e06516aeec2aa690ab775b4a8ef35ef1f17088
Yama-Tomo <mazda13bturbo@gmail.com>
2019-06-14 17:43:42 +0900

    feat: 検索のユースケースを呼び出すHTTPの入り口を作成

---
+++ __api/app/controllers/employees_controller.rb
@@ -0,0 +1,24 @@
+class EmployeesController < ActionController::Base
+  def index
+    result = SearchEmployees.new(params).execute
+
+    data_sets = result[:data_sets].map do |row|
+      belongs = row.branches.reduce({}) do |memo, branch|
+        memo[branch.group.id] ||= { group: branch.group.name, branches: [] }
+        memo[branch.group.id][:branches] << branch.name
+        memo
+      end.map { |(_k, v)| v }
+
+      {
+        id: row.id,
+        first_name: row.first_name,
+        last_name: row.last_name,
+        sex: row.sex.name,
+        country: row.country.name,
+        belongs: belongs,
+      }
+    end
+
+    render :json => { data_sets: data_sets, total_count: result[:total_count] }
+  end
+end
