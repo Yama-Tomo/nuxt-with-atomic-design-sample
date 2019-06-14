31fc62bc2f7914dcc2cbe0f4756f1d82be371174
Yama-Tomo <mazda13bturbo@gmail.com>
2019-06-14 10:59:08 +0900

    chore: ファイルのリネームに伴いモジュール名の修正

--- src/store_modules/module_mapper.ts
+++ src/store_modules/module_mapper.ts
@@ -1,14 +1,14 @@
 import * as SearchEmployee from './search/employee';
 
 // eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
-export namespace modules.search.employer {
-  export const conditions = 'search#employer#conditions';
+export namespace modules.search.employee {
+  export const conditions = 'search#employee#conditions';
 }
 
 export interface StateMapper {
-  [modules.search.employer.conditions]: SearchEmployee.IState;
+  [modules.search.employee.conditions]: SearchEmployee.IState;
 }
 
 export interface ActionMapper {
-  [modules.search.employer.conditions]: SearchEmployee.IActions;
+  [modules.search.employee.conditions]: SearchEmployee.IActions;
 }
