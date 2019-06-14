1f9f706fc28c21d4d422faae23466effddc7812f
Yama-Tomo <mazda13bturbo@gmail.com>
2019-06-14 17:33:36 +0900

    feat: relation追加

--- __api/app/models/employee.rb
+++ __api/app/models/employee.rb
@@ -5,6 +5,7 @@ class Employee < ApplicationRecord
 
   has_many :employee_branches
   has_many :branches, through: :employee_branches
+  has_many :groups, through: :branches
   accepts_nested_attributes_for :employee_branches
 
   belongs_to :sex
