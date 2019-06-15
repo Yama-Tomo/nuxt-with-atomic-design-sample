20b6dd104447a103aac95c19ab6f67d8f5dfec92
Yamatomo <mazda13bturbo@gmail.com>
2019-06-15 15:36:24 +0900

    feat: 初期描画用のユースケースを呼び出すHTTPの入り口を作成

---
+++ __api/app/controllers/bff/employees_controller.rb
@@ -0,0 +1,33 @@
+module Bff
+  class EmployeesController < ActionController::Base
+    def index
+      result = Bff::SearchEmployees.new(params).execute
+
+      data_sets = result[:data_sets].map do |row|
+        belongs = row.branches.reduce({}) do |memo, branch|
+          memo[branch.group.id] ||= { group: branch.group.name, branches: [] }
+          memo[branch.group.id][:branches] << branch.name
+          memo
+        end.map { |(_k, v)| v }
+
+        {
+          id: row.id,
+          first_name: row.first_name,
+          last_name: row.last_name,
+          sex: row.sex.name,
+          country: row.country.name,
+          belongs: belongs,
+        }
+      end
+
+      render :json => {
+        data_sets: data_sets,
+        total_count: result[:total_count],
+        groups: result[:groups].map { |g| g.slice(:id, :name) },
+        branches: result[:branches].map { |g| g.slice(:id, :name) },
+        sex: result[:sex].map { |sex| sex.attributes.slice(:id, :name) },
+        countries: result[:countries].map { |country| country.attributes.slice(:id, :name) },
+      }
+    end
+  end
+end
--- __api/config/routes.rb
+++ __api/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :employees, only: %i[index]
+
+  namespace :bff do
+    resources :employees, only: %i[index]
+  end
 end
