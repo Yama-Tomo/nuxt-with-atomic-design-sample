8e91f9417378cda4ee58239674a61d250c390310
Yamatomo <mazda13bturbo@gmail.com>
2019-06-16 19:56:31 +0900

    feat: 部署の一覧を返すAPIをたたくactionを追加

--- src/store_modules/employee/actions.ts
+++ src/store_modules/employee/actions.ts
@@ -1,15 +1,30 @@
 import { DefineActions } from 'vuex-type-helper';
+import { NuxtAxiosInstance } from '@nuxtjs/axios';
 import { IState } from './state';
 import { IMutations } from './mutations';
+import { Request } from '@/repositories/network/api/branches/get';
 
 export interface IActions {
   resetState: IMutations['resetState'];
   setAttribute: IMutations['setAttribute'];
+  updateBranches: { axios: NuxtAxiosInstance; groupId: number };
 }
 
 export const actions: DefineActions<IActions, IState, IMutations> = {
   resetState: ({ commit }) => commit('resetState', undefined),
   setAttribute: ({ commit }, args) => commit('setAttribute', args),
+  updateBranches: async ({ commit }, args) => {
+    const request = new Request(args.axios);
+    const data = await request.send({ groupId: args.groupId });
+
+    commit('setAttribute', {
+      key: 'branches',
+      val: data.map(d => ({
+        label: d.name,
+        value: d.id,
+      })),
+    });
+  },
 };
 
 export default actions;
--- src/utils/camelize.ts
+++ src/utils/camelize.ts
@@ -1,4 +1,5 @@
 import camelCase from 'lodash/camelCase';
+import snakeCase from 'lodash/snakeCase';
 
 type AnyObject = { [key: string]: any };
 
@@ -16,6 +17,23 @@ export const camelizeKeysDeep = (value: unknown): any => {
       key => (camelizeKeysObject[camelCase(key)] = camelizeKeysDeep(value[key]))
     );
     return camelizeKeysObject;
+  }
+
+  return value;
+};
+
+export const underscoreKeysDeep = (value: unknown): any => {
+  if (Array.isArray(value)) {
+    return value.map(element => underscoreKeysDeep(element));
+  }
+
+  if (isAnyObject(value)) {
+    const underscoreKeysObject = {} as AnyObject;
+    Object.keys(value).forEach(
+      key =>
+        (underscoreKeysObject[snakeCase(key)] = underscoreKeysDeep(value[key]))
+    );
+    return underscoreKeysObject;
   }
 
   return value;
