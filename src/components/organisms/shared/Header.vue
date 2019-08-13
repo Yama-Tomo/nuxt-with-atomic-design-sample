8a2855cebf13d9c71563c57388892cf52871590b
Yama-Tomo <mazda13bturbo@gmail.com>
2019-08-13 13:31:03 +0900

    feat: headerとfooterを作成

---
+++ src/components/organisms/shared/Header.vue
@@ -0,0 +1,41 @@
+<script lang="tsx">
+import * as vts from 'vue-tsx-support';
+import { Component, Vue } from 'nuxt-property-decorator';
+import { VToolbar, VToolbarItems, VIcon, VBtn } from 'vuetify-tsx';
+
+@Component
+class Header extends Vue {
+  render() {
+    return (
+      <VToolbar dark color="#24292e">
+        <VToolbarItems>
+          <VIcon large>people</VIcon>
+          <VBtn flat>Employees</VBtn>
+          <VBtn flat>Groups</VBtn>
+          <VBtn flat>Branches</VBtn>
+        </VToolbarItems>
+      </VToolbar>
+    );
+  }
+}
+
+export default vts.ofType<{}, {}, {}>().convert(Header);
+</script>
+
+<style lang="scss" scoped>
+.v-toolbar__items {
+  .v-btn {
+    text-transform: inherit;
+
+    &:first-child {
+      min-width: 0;
+      padding-right: 0;
+      padding-left: 0;
+    }
+
+    &.v-btn--flat:nth-child(2) {
+      margin-left: 16px;
+    }
+  }
+}
+</style>
