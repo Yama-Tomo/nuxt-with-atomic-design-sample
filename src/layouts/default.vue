6fb8e20060c0f545f26b7aea051a713507cb4cd0
Yama-Tomo <mazda13bturbo@gmail.com>
2019-08-13 13:31:39 +0900

    feat: レイアウトを作成

---
+++ src/layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <Header />
+    <v-content>
+      <v-container fluid>
+        <router-view />
+      </v-container>
+    </v-content>
+    <Footer />
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+import Header from '@/components/organisms/shared/Header.vue';
+import Footer from '@/components/organisms/shared/Footer.vue';
+
+@Component({
+  components: {
+    Header,
+    Footer,
+  },
+})
+export default class DefaultLayout extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.application.theme--light {
+  background-color: white;
+}
+</style>
